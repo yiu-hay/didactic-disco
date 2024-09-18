9f2c9faeb380e6b61a98813e903356b3425d9bc7
yiu-hay <48990615+yiu-hay@users.noreply.github.com>
2024-09-18 13:49:28 +0100

    Update simple_business.sh

--- simple_business.sh
+++ simple_business.sh
@@ -4,6 +4,8 @@ echo Last 3 incidents:
 echo
 curl -s https://opsgenie.status.atlassian.com/api/v2/incidents.json | jq -r '(.incidents[:3] | map({name, created_at, status}))'
 
+
+
 # Extra Time
 echo Current Systems Status: $(curl -s https://opsgenie.status.atlassian.com/api/v2/status.json | jq -r '.status.description')
 echo
